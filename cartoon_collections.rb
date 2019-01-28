7df58a21cb6084a820a08f7b2b3e4aa662633508
Allen F Partlow <allenfp@gmail.com>
2019-01-28 02:46:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
 def roll_call_dwarves(array)
-    array.each_with_index {|element, index| puts "#{index+1}.*#{element}"}
+  array.each_with_index {|element, index| puts "#{index+1}.*#{element}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect {|element|
+     element.capitalize
+     element += "!"
+   }
 end
 
 def long_planeteer_calls# code an argument here
