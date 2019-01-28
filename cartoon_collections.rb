bf1e250fd061a114203a56f9513d5b098d18b679
Allen F Partlow <allenfp@gmail.com>
2019-01-28 02:47:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,13 +4,13 @@ end
 
 def summon_captain_planet(array)
   array.collect {|element|
-     element.capitalize
+     element.capitalizes
      element += "!"
    }
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+
 end
 
 def find_the_cheese# code an argument here
