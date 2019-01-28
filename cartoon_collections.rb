fb6c74af412574e3276f6cce378a1acffc22d1bf
Allen F Partlow <allenfp@gmail.com>
2019-01-28 02:52:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,10 @@ def summon_captain_planet(array)
    }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
+  array.include? { |element|
+      element.length <= 4
+  }
 
 end
 
